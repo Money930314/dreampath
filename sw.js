9ef66f38a8354508f2b7a060b90f2f575b2af3b5
Money930314 <zhaoyun20031023@gmail.com>
2025-11-03 23:32:35 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = "dreampath-cache-v2";
+const ASSETS = ["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install", (e)=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(self.skipWaiting())); });
+self.addEventListener("activate", (e)=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))); self.clients.claim(); });
+self.addEventListener("fetch", (e)=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
